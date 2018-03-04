e0d187101ded66be6a447a60cf10c188072c1049
Christopher Moore <christopher.moore@purepoint.io>
2018-03-04 11:07:46 -0500

    add frequencies to Sequence

--- swift-additions.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
+++ swift-additions.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
@@ -8,3 +8,15 @@ extension Array {
 
 let testArray = [1, 2, 3]
 testArray[safe: 7]
+
+extension Sequence where Element: Hashable {
+
+    var frequencies: [Element: Int] {
+        let frequencyPairs = self.map { ($0, 1) }
+        return Dictionary(frequencyPairs, uniquingKeysWith: +)
+    }
+}
+
+let frequecies = "hello".frequencies
+let onlyOne = frequecies.filter { $0.value == 1 }
+print(onlyOne)
