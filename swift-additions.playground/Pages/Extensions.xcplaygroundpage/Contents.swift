63cedf9d3ee5c52c2275185db73a4640110b3dba
Christopher Moore <christopher.moore@purepoint.io>
2018-03-04 15:34:15 -0500

    add uniques to Sequence

--- swift-additions.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
+++ swift-additions.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
@@ -15,8 +15,22 @@ extension Sequence where Element: Hashable {
         let frequencyPairs = self.map { ($0, 1) }
         return Dictionary(frequencyPairs, uniquingKeysWith: +)
     }
+
+    func uniques() -> [Element] {
+        var seen: Set<Element> = []
+        return filter { element in
+            if seen.contains(element) {
+                return false
+            } else {
+                seen.insert(element)
+                return true
+            }
+        }
+    }
 }
 
 let frequecies = "hello".frequencies
 let onlyOne = frequecies.filter { $0.value == 1 }
 print(onlyOne)
+
+let uniques = [1, 2, 3, 12, 1, 3, 4, 5, 6, 4, 6].uniques()
